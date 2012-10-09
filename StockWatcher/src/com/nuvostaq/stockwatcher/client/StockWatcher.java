010b68dd4247e9ee93d852aa0fe4b59f0f459f11
juha <juha.hakkinen@nuvostaq.com>
2012-10-09 11:21:50 +0300

    Internationalizing a GWT Application completed

--- StockWatcher/src/com/nuvostaq/stockwatcher/client/StockWatcher.java
+++ StockWatcher/src/com/nuvostaq/stockwatcher/client/StockWatcher.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Date;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.Random;
@@ -32,16 +33,23 @@ public class StockWatcher implements EntryPoint {
 	private Button addStockButton = new Button("Add");
 	private Label lastUpdatedLabel = new Label();
 	private ArrayList<String> stocks = new ArrayList<String>();
+	private StockWatcherConstants constants = GWT.create(StockWatcherConstants.class);
+	private StockWatcherMessages messages = GWT.create(StockWatcherMessages.class);
 
 	/**
 	 * Entry point method.
 	 */
 	public void onModuleLoad() {
+	    // Set the window title, the header text, and the Add button text.
+	    Window.setTitle(constants.stockWatcher());
+	    RootPanel.get("appTitle").add(new Label(constants.stockWatcher()));
+	    addStockButton = new Button(constants.add());
+
 		// Create table for stock data.
-		stocksFlexTable.setText(0, 0, "Symbol");
-		stocksFlexTable.setText(0, 1, "Price");
-		stocksFlexTable.setText(0, 2, "Change");
-		stocksFlexTable.setText(0, 3, "Remove");
+		stocksFlexTable.setText(0, 0, constants.symbol());
+		stocksFlexTable.setText(0, 1, constants.price());
+		stocksFlexTable.setText(0, 2, constants.change());
+		stocksFlexTable.setText(0, 3, constants.remove());
 		
 		// Add styles to elements in the stock list table.
 	    stocksFlexTable.setCellPadding(6);
@@ -101,7 +109,7 @@ public class StockWatcher implements EntryPoint {
 
 		// Stock code must be between 1 and 10 chars that are numbers, letters, or dots.
 		if (!symbol.matches("^[0-9A-Z\\.]{1,10}$")) {
-			Window.alert("'" + symbol + "' is not a valid symbol.");
+			Window.alert(messages.invalidSymbol(symbol));
 			newSymbolTextBox.selectAll();
 			return;
 		}
@@ -164,8 +172,7 @@ public class StockWatcher implements EntryPoint {
 			updateTable(prices[i]);
 		}
 		// Display timestamp showing last refresh.
-		lastUpdatedLabel.setText("Last update : "
-				+ DateTimeFormat.getMediumDateTimeFormat().format(new Date()));
+	    lastUpdatedLabel.setText(messages.lastUpdate(new Date()));
 	}
 	/**
 	 * Update a single row in the stock table.
